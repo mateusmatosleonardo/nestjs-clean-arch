9397f91be517a8d16496331b9c94632ce0fc333e
mateus <m4teusmatos@gmail.com>
2023-07-31 20:59:37 -0300

    validator email field

--- src/users/domain/validators/__tests__/unit/user.validator.spec.ts
+++ src/users/domain/validators/__tests__/unit/user.validator.spec.ts
@@ -12,6 +12,14 @@ describe('UserValidator unit tests', () => {
     sut = UserValidatorFactory.create();
   });
 
+  it('Valid cases for user validator class', () => {
+    const props = UserDataBuilder({});
+    const isValid = sut.validate(props);
+
+    expect(isValid).toBeTruthy();
+    expect(sut.validatedData).toStrictEqual(new UserRoles(props));
+  });
+
   describe('Name field', () => {
     it('Invalidation cases for name field', () => {
       let isValid = sut.validate(null as any);
@@ -39,13 +47,43 @@ describe('UserValidator unit tests', () => {
         'name must be shorter than or equal to 255 characters',
       ]);
     });
+  });
 
-    it('Valid cases for name field', () => {
-      const props = UserDataBuilder({});
-      const isValid = sut.validate(props);
+  describe('Email field', () => {
+    it('Invalidation cases for email field', () => {
+      let isValid = sut.validate(null as any);
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['email']).toStrictEqual([
+        'email must be an email',
+        'email should not be empty',
+        'email must be a string',
+        'email must be shorter than or equal to 255 characters',
+      ]);
 
-      expect(isValid).toBeTruthy();
-      expect(sut.validatedData).toStrictEqual(new UserRoles(props));
+      isValid = sut.validate({ ...UserDataBuilder({}), email: '' as any });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['email']).toStrictEqual([
+        'email must be an email',
+        'email should not be empty',
+      ]);
+
+      isValid = sut.validate({ ...UserDataBuilder({}), email: 12 as any });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['email']).toStrictEqual([
+        'email must be an email',
+        'email must be a string',
+        'email must be shorter than or equal to 255 characters',
+      ]);
+
+      isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256),
+      });
+      expect(isValid).toBeFalsy();
+      expect(sut.errors['email']).toStrictEqual([
+        'email must be an email',
+        'email must be shorter than or equal to 255 characters',
+      ]);
     });
   });
 });
