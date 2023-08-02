32ef419085c4985a723cead80f17dd65c9b0e9a2
mateus <m4teusmatos@gmail.com>
2023-08-01 22:19:23 -0300

    integration tests - validate name field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -5,9 +5,23 @@ import { UserEntity, UserProps } from '../../user.entity';
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
     it('Should throw an error when creating a user with invalid name', () => {
-      const props: UserProps = {
+      let props: UserProps = {
         ...UserDataBuilder({}),
         name: null,
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: '',
+      };
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 'a'.repeat(256),
       };
 
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
